9e00bf718c825e37424d14821a598c06c784be2a
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-07-11 01:30:04 +0700

    Diploma. Stage 4. Footer layout ready.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,28 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <h2>Футер</h2>
+    <div className='footer'>
+      <div className='footer__container'>
+        <h3 className='footer__title'>
+          Учебный проект Яндекс.Практикум х BeatFilm.
+        </h3>
+        <div className='footer__copyright-and-links'>
+          <p className='footer__copyright'>&copy; 2023</p>
+          <ul className='footer__links'>
+            <li className='footer__item'>
+              <a className='footer__link' href='/'>
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li className='footer__item'>
+              <a className='footer__link' href='/'>
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 };
 
