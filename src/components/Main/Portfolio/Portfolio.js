1c4cf40282c1f60062a279478db1c2bd05943ef3
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-07-11 00:35:47 +0700

    Diploma. Stage 4. Portfolio layout ready.

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -3,7 +3,22 @@ import './Portfolio.css';
 
 const Portfolio = () => {
   return (
-    <h2>Портфолио</h2>
+    <div className='portfolio'>
+      <div className='portfolio__container'>
+        <h2 className='portfolio__title'>Портфолио</h2>
+        <ul className='portfolio__links'>
+          <li className='portfolio__item'>
+            <a className='portfolio__link' href='/'>Статичный сайт</a>
+          </li>
+          <li className='portfolio__item'>
+            <a className='portfolio__link' href='/'>Адаптивный сайт</a>
+          </li>
+          <li className='portfolio__item'>
+            <a className='portfolio__link' href='/'>Одностраничное приложение</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 };
 
