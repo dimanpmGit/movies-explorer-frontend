196969aef7a22920ba9534f7a48b21a311cfc289
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-07-11 04:27:58 +0700

    Diploma. Stage 4. Main page. Search string layout ready.

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Movies.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import SearchForm from '../SearchForm/SearchForm';
 
 const Movies = () => {
   return (
     <>
       <Header />
+      <SearchForm />
       <Footer />
     </>
   )
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,7 +3,26 @@ import './SearchForm.css';
 
 const SearchForm = () => {
   return (
-    <h2>форма поиска, куда пользователь будет вводить запрос</h2>
+    <form className='search-form'>
+      <div className='search-form__container'>
+        <div className='search-form__wrapper'>
+          <input className='search-form__input' type='text' name='film' placeholder='Фильм'></input>
+          <button className='search-form__button' type='submit'>Поиск</button>
+        </div>
+        <div className='search-form__short-films-wrapper'>
+          <div className='search-form__short-films'>
+            <input className='search-form__short-films-btn' type='checkbox' id='short-films'/>
+            <div className='search-form__sliding-btn'></div>
+            <label for='short-films' className='search-form__short-films' name='short-films'>
+              <p className='search-form__short-films-label'>
+                  Короткометражки
+                </p>
+              </label>
+          </div>
+        </div>
+        <div className='search-form__hr'></div>
+      </div>
+    </form>
   )
 };
 
