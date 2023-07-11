53d5fd97488364f422d74904ebf433f698217b2b
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-07-11 19:28:50 +0700

    Diploma. Stage 4. 404 page layout ready.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
+import NotFound from '../NotFound/NotFound';
 
 import './App.css';
 
@@ -19,6 +20,7 @@ function App() {
         <Route path='/profile' element={<Profile />} />
         <Route path='/signin' element={<Login />} />
         <Route path='/signup' element={<Register />} />
+        <Route path='/not-found' element={<NotFound />} />
       </Routes>
     </div>
   );
