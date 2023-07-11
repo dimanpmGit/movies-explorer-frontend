9902d2b677a0de21c67ee8b7c025241197deeb63
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-07-12 03:18:58 +0700

    Diploma. Stage 4. 768 Movies page layout ready.

--- src/components/AuthMenu/AuthMenu.js
+++ src/components/AuthMenu/AuthMenu.js
@@ -3,12 +3,12 @@ import './AuthMenu.css';
 
 const AuthMenu = () => {
   return (
-    <ul className='authmenu'>
-      <li className='authmenu__item'>
-        <a className='authmenu__link' href='/signup'>Регистрация</a>
+    <ul className='auth-menu'>
+      <li className='auth-menu__item'>
+        <a className='auth-menu__link' href='/signup'>Регистрация</a>
       </li>
-      <li className='authmenu__item'>
-        <a className='authmenu__link' href='/signin'>Войти</a>
+      <li className='auth-menu__item'>
+        <a className='auth-menu__link' href='/signin'>Войти</a>
       </li>
     </ul>
   )
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,19 +4,22 @@ import './Navigation.css';
 const Navigation = () => {
   return (
     <div className='navigation'>
-      <ul className='navigation__films'>
-        <li className='navigation__item'>
-          <a className='navigation__item-link navigation__item-link_active app__link' href='/movies'>
-            Фильмы
-          </a>
-        </li>
-        <li className='navigation__item'>
-          <a className='navigation__item-link app__link' href='/saved-movies'>
-            Сохранённые фильмы
-          </a>
-        </li>
-      </ul>
-      <a className='navigation__profile-link app__link' href='/profile'>Аккаунт</a>
+      <div className='navigation__wrapper'>
+        <ul className='navigation__films'>
+          <li className='navigation__item'>
+            <a className='navigation__item-link navigation__item-link_active app__link' href='/movies'>
+              Фильмы
+            </a>
+          </li>
+          <li className='navigation__item'>
+            <a className='navigation__item-link app__link' href='/saved-movies'>
+              Сохранённые фильмы
+            </a>
+          </li>
+        </ul>
+        <a className='navigation__profile-link app__link' href='/profile'>Аккаунт</a>
+      </div>
+      <div className='navigation__hamburger'></div>
     </div>
   )
 };
